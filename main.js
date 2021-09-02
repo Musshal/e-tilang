cb8eaef563aa0642f37c076f5041a17c9ea900fb
Muhammad Rivan <rivanfebrian123@gmail.com>
2021-09-02 10:30:41 +0700

    update

--- main.js
+++ main.js
@@ -26,31 +26,34 @@ const searchBook = () => {
   const searchBook = inputSearchBook.value.toLowerCase();
   const books = document.querySelectorAll(".book_item");
 
-  Array.from(books).forEach((book) => {
-    const textTitle = book.firstElementChild.textContent;
-    if (textTitle.toLowerCase().indexOf(searchBook) != -1) {
+  for (book of books) {
+    const textTitle = book.children[0].textContent.toLowerCase();
+    if (textTitle.indexOf(searchBook) != -1) {
       book.style.display = "block";
     } else {
       book.style.display = "none";
     }
-  });
+  }
 }
 
 xml.onload = () => {
   const bookList = document.getElementById('bookList');
-  var workbook = XLSX.read(xml.response, {type: 'array'});
+  var workbook = XLSX.read(xml.response, {
+    type: 'array'
+  });
   var result = [];
 
-  workbook.SheetNames.forEach((name) => {
-    result.push(...XLSX.utils.sheet_to_json(workbook.Sheets[name], {header: 1}));
-  });
+  for (name of workbook.SheetNames) {
+    result.push(...XLSX.utils.sheet_to_json(workbook.Sheets[name], {
+      header: 1
+    }));
+  }
 
-  result.forEach((i) => {
+  for (i of result) {
     if (typeof i[0] === 'number' && typeof i[3] === 'string') {
-
       bookList.append(makeBook(i[3], i[4], i[0]))
     }
-  })
+  }
 }
 
 xml.onreadystatechange = () => {
